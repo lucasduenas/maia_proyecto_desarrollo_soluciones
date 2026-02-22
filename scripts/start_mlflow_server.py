57d66a78febe4c703f166c9b552f17bdca0bf238
Whitman Rojas <whitman.rojas@blend360.com>
2026-02-21 22:09:57 -0500

    fix backing store

--- scripts/start_mlflow_server.py
+++ scripts/start_mlflow_server.py
@@ -90,7 +90,7 @@ def iniciar_servidor_mlflow(
         'mlflow', 'server',
         '--host', host,
         '--port', str(port),
-        '--backend-store-uri', backend_store,
+        # '--backend-store-uri', backend_store,
         '--default-artifact-root', artifact_root,
         '--allowed-hosts', f'{allowed_hosts[0]}, {allowed_hosts[1]}, {allowed_hosts[2]}',
         '--cors-allowed-origins', "*",
